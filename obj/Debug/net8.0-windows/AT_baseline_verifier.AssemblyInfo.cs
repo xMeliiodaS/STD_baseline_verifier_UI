427f922a383f8ae758bf1450921c51b0f61c1817
xMeliiodaS <yourgithub@email.com>
2025-08-09 13:49:29 +0300

    Initial commit - WPF UI project setup

--- obj/Debug/net8.0-windows/AT_baseline_verifier.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/AT_baseline_verifier.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AT_baseline_verifier")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+974d114797e288b60c83d4e848e8c9750838f87c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50ebb94940780b780910bfc49d675dc0e56829db")]
 [assembly: System.Reflection.AssemblyProductAttribute("AT_baseline_verifier")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AT_baseline_verifier")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
