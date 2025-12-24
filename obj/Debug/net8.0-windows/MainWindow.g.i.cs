e3ca83fe85b9b018a466ba8e8fca4557c9412f6f
xMeliiodaS <yourgithub@email.com>
2025-12-24 19:57:59 +0200

    Add new main window that contains buttons to access tools

--- obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -239,7 +239,7 @@ namespace AT_baseline_verifier {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AT_baseline_verifier;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AT_baseline_verifier;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
